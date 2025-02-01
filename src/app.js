6f23f3a2fd3236432036b7346ad95ee96c007aef
ACarr0ll <42918684+ACarr0ll@users.noreply.github.com>
2025-02-01 17:49:01 -0600

    test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
-const db = require('./db'); // Add this line
+const authMiddleware = require('./middleware/auth');
+const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(authMiddleware);
 
 // Routes
 routes(app);
