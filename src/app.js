359e6372ee18d05e89a721259b94e2c211cf6ca0
ACarr0ll <42918684+ACarr0ll@users.noreply.github.com>
2025-02-01 17:37:31 -0600

    testing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
-const authMiddleware = require('./middleware/auth');
+const db = require('./db'); // Add this line
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(authMiddleware); // Add this line
 
 // Routes
 routes(app);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,50 @@
-function setRoutes(app) {
-    const { getIndex } = require('../controllers/index');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('../db');
+const router = express.Router();
 
-    app.get('/', getIndex);
-}
+router.get('/login', (req, res) => {
+    res.render('login');
+});
 
-module.exports = setRoutes;
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
+        if (err) throw err;
+
+        if (results.length > 0) {
+            const user = results[0];
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) throw err;
+
+                if (isMatch) {
+                    // Set session and redirect to homepage
+                    req.session.user = user;
+                    res.redirect('/');
+                } else {
+                    res.send('Incorrect password');
+                }
+            });
+        } else {
+            res.send('User not found');
+        }
+    });
+});
+
+router.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    bcrypt.hash(password, 10, (err, hash) => {
+        if (err) throw err;
+
+        db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, hash], (err, results) => {
+            if (err) throw err;
+            res.redirect('/login');
+        });
+    });
+});
+
+module.exports = (app) => {
+    app.use('/', router);
+};
