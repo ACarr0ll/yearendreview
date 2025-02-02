bddd26d874b14876e68b24abe9db66e4ceaf25f1
ACarr0ll <42918684+ACarr0ll@users.noreply.github.com>
2025-02-01 19:00:08 -0600

    fixed index.ejs.

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,42 +7,40 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
-    db.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
-        if (err) throw err;
-
-        if (results.length > 0) {
-            const user = results[0];
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-                if (err) throw err;
-
-                if (isMatch) {
-                    // Set session and redirect to homepage
-                    req.session.user = user;
-                    res.redirect('/');
-                } else {
-                    res.send('Incorrect password');
-                }
-            });
+    try {
+        const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+        if (result.rows.length > 0) {
+            const user = result.rows[0];
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (isMatch) {
+                req.session.user = user;
+                res.redirect('/');
+            } else {
+                res.send('Incorrect password');
+            }
         } else {
             res.send('User not found');
         }
-    });
+    } catch (err) {
+        console.error('Error querying the database:', err.stack);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err) throw err;
-
-        db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, hash], (err, results) => {
-            if (err) throw err;
-            res.redirect('/login');
-        });
-    });
+    try {
+        const hash = await bcrypt.hash(password, 10);
+        await db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hash]);
+        res.redirect('/login');
+    } catch (err) {
+        console.error('Error inserting into the database:', err.stack);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = (app) => {
