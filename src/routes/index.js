8b422741ebfa820417e36b8269727070669bdca7
ACarr0ll <42918684+ACarr0ll@users.noreply.github.com>
2025-02-01 19:13:03 -0600

    Testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -34,6 +34,10 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
 router.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
