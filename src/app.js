6cc88bfe242f163e9af76daf672c8042a92431f4
ACarr0ll <42918684+ACarr0ll@users.noreply.github.com>
2025-02-02 11:37:11 -0600

    Hide Username password

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public'))); // Ensure this path is correct
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
